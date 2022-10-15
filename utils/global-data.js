33d5ffb44266c59f1dc8cd41bc0d90dcb34ae096
Ismoil Shokirov <ismoil.793@gmail.com>
2022-10-15 15:38:29 +0500

    changed default BLOG_TITLE

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Kirov logs';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Welcome to Ismoil Shokirov\'s blog';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
