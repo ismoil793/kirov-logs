ee32fa0a0154578a5d530cda6b9bfc463e75b995
Ismoil Shokirov <ismoil.793@gmail.com>
2022-10-15 22:43:12 +0500

    SEO updated

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,14 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+
+      <meta name="author" content="Ismoil Shokirov" />
+      <meta name="keywords" content={"Ismoil Shokirov blog, ReactJS, NextJS, Software Engineer, Uzbekistan"} />
+      <meta property="og:image" content={"/kirov.jpg"} />
+      <meta property="og:type" content={"website"} />
+      <meta property="og:site_name" content="blog.shokirov.uz" />
+      <meta property="og:url" content={"https://blog.shokirov.uz"} />
     </Head>
   );
 }
